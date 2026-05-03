588bcd2cedee72ba89e930421e35cc5e79a10efd
James Supeyo <jsupeyo@gmail.com>
2026-05-03 10:00:36 -0400

    Removed the variables that had a high collinearity...the analysis has to be redone again.

--- mie_622_group_project/code/project_code.R
+++ mie_622_group_project/code/project_code.R
@@ -1,4 +1,4 @@
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # set up the libraries
 library(tidyverse)
 library(readxl) 
@@ -17,7 +17,7 @@ library(janitor) # to clean messy column names
 library(knitr) # to convert from qmd to r script
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # load the data
 pj_data <- read_xlsx(here("data", "project_data.xlsx")) 
 # the data file is in a folder named 'data'
@@ -26,7 +26,7 @@ str(pj_data)
 summary(pj_data)
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # clean messy column names
 pj_data <- clean_names(pj_data) 
 
@@ -34,7 +34,11 @@ pj_data <- clean_names(pj_data)
 pj_data <- na.omit(pj_data) 
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+colnames(pj_data)
+
+
+## -----------------------------------------------------------------------------
 # Factorize the categorical variables
 # 'target' is the only categorical variable
 col <- c("target")
@@ -44,7 +48,7 @@ levels(pj_data$target)
 table(pj_data$target)
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Let's take a look at the distribution of our variable of interest
 pj_data |>
   ggplot(aes(x =target, fill = target))+
@@ -55,7 +59,7 @@ pj_data |>
         y = "Count")
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # set reference level
 pj_data$target <- relevel(pj_data$target, ref = "Graduate" )
 
@@ -63,29 +67,29 @@ pj_data$target <- relevel(pj_data$target, ref = "Graduate" )
 levels(pj_data$target)
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 set.seed(123)  # setting a seed for reproducibility of the random split
 index <- createDataPartition(pj_data$`target`, p = 0.3, list = FALSE)
 training_data <- pj_data[-index, ]
 testing_data <- pj_data[index, ]
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 summary(training_data$`target`)
 summary(testing_data$`target`)
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 cat("Training rows:", nrow(training_data), "\n")
 cat("Testing rows:", nrow(testing_data), "\n")
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 prop.table(table(training_data$target))
 prop.table(table(testing_data$target))
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # We first fit a temporary linear model just to extract VIF values - this 
 # is a common workaround since vif() requires a linear model object
 temp_model <- lm(as.numeric(target) ~ ., data = training_data)
@@ -93,14 +97,37 @@ vif_values <- vif(temp_model) # get the vif values from the model
 vif_values[vif_values > 5] # show variables that have vif values greater 
 # than 5 (potential collinearity issue)
 
+# remove the flagged variables from both training and testing data sets
+training_data <- training_data |> select (-c("mothers_occupation", 
+                                             "fathers_occupation",
+                                             "curricular_units_1st_sem_credited",
+                                             "curricular_units_1st_sem_enrolled",
+                                             "curricular_units_1st_sem_approved",
+                                             "curricular_units_1st_sem_grade",
+                                             "curricular_units_2nd_sem_credited",
+                                             "curricular_units_2nd_sem_enrolled",
+                                             "curricular_units_2nd_sem_approved",
+                                             "curricular_units_2nd_sem_grade"
+                                             ))
+testing_data <- testing_data |> select(-c("mothers_occupation", 
+                                             "fathers_occupation",
+                                             "curricular_units_1st_sem_credited",
+                                             "curricular_units_1st_sem_enrolled",
+                                             "curricular_units_1st_sem_approved",
+                                             "curricular_units_1st_sem_grade",
+                                             "curricular_units_2nd_sem_credited",
+                                             "curricular_units_2nd_sem_enrolled",
+                                             "curricular_units_2nd_sem_approved",
+                                             "curricular_units_2nd_sem_grade"))
 
-## ------------------------------------------------------------------------------------------------------------------------
+
+## -----------------------------------------------------------------------------
 model_0 <- multinom(target~., data = training_data, maxit = 500)
 # maxit = 500 gives the optimizer more iterations to converge
 summary(model_0)
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # model fit: 
 AIC(model_0) # lower AIC is better
 
@@ -112,7 +139,7 @@ cat("McFadden's Pseudo R²:", round(as.numeric(mcfadden_r2), 4), "\n") # print
 # the pseudo R squared value
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # make predictions on the test data
 predicted_class <- predict(model_0, newdata = testing_data) 
 
@@ -121,13 +148,13 @@ conf_matrix <- confusionMatrix(predicted_class, testing_data$target)
 conf_matrix
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Relative Risk Ratios: exp(coefficients)
 rrr <- exp(coef(model_0))
 rrr
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 importance <- varImp(model_0)
 # rank the variables by importance
 importance_df <- importance |>
@@ -150,7 +177,7 @@ importance_df |>
 
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # classification tree model
 model1 <- rpart(formula = target ~ .-target, 
                 data= training_data, 
@@ -160,7 +187,7 @@ rpart.plot(model1)
 head(training_data)
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # make predictions on the test data
 predicted_class_1 <- predict(model1, newdata = testing_data) 
 
@@ -175,17 +202,19 @@ conf_matrix_1
 
 
 
-## ------------------------------------------------------------------------------------------------------------------------
-vip(model1)
+## -----------------------------------------------------------------------------
+vip(model1)+
+  geom_col(fill="steelblue")+
+  theme_minimal()
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 model2 <-rpart(formula = target ~ ., data = training_data, method = "anova")
 # we use 'anova' when developing a regression tree
 rpart.plot(model2)
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # make predictions on the test data
 predicted_class_2 <- predict(model2, newdata = testing_data)
 
@@ -203,22 +232,24 @@ rss <- sum((target_num - pred_num)^2)
 rss
 
 
-## ------------------------------------------------------------------------------------------------------------------------
-vip(model2)
+## -----------------------------------------------------------------------------
+vip(model2)+
+  geom_col(fill = "steelblue")+
+  theme_minimal()
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 plotcp(model2)
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 pruned1 <- rpart(formula = target ~ .,data= training_data, method  = "anova", 
                  control = list(cp = 0.022, xval = 10)) 
                   # xval = 10 is a common validation number
 rpart.plot(pruned1)
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 pruned2 <- rpart(formula = target ~ .,data= training_data, 
                  method  = "anova",  
                  control = list(cp = 0.014, 
@@ -226,7 +257,7 @@ pruned2 <- rpart(formula = target ~ .,data= training_data,
 rpart.plot(pruned2)
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 rf_model1 <- randomForest(target ~ ., data=training_data, 
                           mtry = 8, importance=TRUE)
 
@@ -237,11 +268,11 @@ rf_model1 <- randomForest(target ~ ., data=training_data,
 rf_model1 
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 table(training_data$target)
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Let's set the sample size based on our smallest class (Enrolled = 555)
 # This forces the model to treat all classes with equal weight
 rf_model1_balanced <- randomForest(
@@ -255,7 +286,7 @@ rf_model1_balanced <- randomForest(
 print(rf_model1_balanced)
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # predict using the balanced random forest model
 predicted_class_3 <- predict(rf_model1_balanced, newdata = testing_data)
 
@@ -264,15 +295,16 @@ brf_cm <- confusionMatrix(predicted_class_3, testing_data$target)
 brf_cm
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 varImpPlot(rf_model1_balanced)
 importance(rf_model1_balanced)
 
 # variable importance plot
-vip(rf_model1_balanced)
+vip(rf_model1_balanced)+
+  geom_col(fill = "steelblue")
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # Let's create a data frame with the Sensitivity scores we've gathered
 comparison_data <- data.frame(
   Model = c("Multinomial Logistic", "Classification Tree", 
@@ -305,6 +337,6 @@ ggplot(plot_data, aes(x = Class, y = Sensitivity, fill = Model)) +
 
 
 
-## ------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 purl("project_code.qmd")
 
